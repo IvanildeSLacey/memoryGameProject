1bdad9f41fb364897f7d834e79d29d5d71c16b41
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-11-29 12:45:51 +0000

    Add items array to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,20 @@ let cards;
 let interval;
 let firstCard = false;
 let secondCard = false;
+
+//Items array
+
+const items = [
+    {name:"bell",image:"bell.jpg" }
+    {name:"decoration",image:"decoration.jpg" }
+    {name:"gingerbread",image:"gingerbread.jpg" }
+    {name:"present",image:"present.jpg" }
+    {name:"presentsanta",image:"presentsanta.jpg" }
+    {name:"reindeer",image:"reindeer.jpg" }
+    {name:"santa",image:"santa.jpg" }
+    {name:"santasheigh",image:"santasleigh.jpg" }
+    {name:"snowman",image:"snowman.jpg" }
+    {name:"tree",image:"tree.jpg" }
+    {name:"xmaself",image:"xmaself.jpg" }
+    {name:"penguim",image:"penguim.jpg" }
+]
